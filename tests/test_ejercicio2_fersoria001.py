6e25f65638234532770ac5b864e140401c41abbb
bercho001 <bercho001@gmail.com>
2022-06-10 11:19:38 -0300

    agregado test de es_maximo y es_minimo

--- tests/test_ejercicio2_fersoria001.py
+++ tests/test_ejercicio2_fersoria001.py
@@ -2,7 +2,7 @@
 # Fernando Agustín Soria - @fersoria001
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-from practica.ejercicio2 import sumar, promediar
+from practica.ejercicio2 import sumar, promediar, es_maximo, es_minimo
 
 """
 Aqui se probaran las funciones correspondientes al ejercicio2
@@ -23,8 +23,34 @@ def test_sumar():
 
 
 def test_promediar():
+    """
+    Se intentará promediar una secuencia de numeros ingresada y retornar
+    un resultado tipo float
+    """
     secuencia = [15,62,-2]
     prueba = promediar(secuencia)
     interno = sum(secuencia) / len(secuencia)
     assert type(prueba) == float, "Requerimos resultado con decimales"
+    assert prueba == interno, "Resultado incorrecto"
+
+
+def test_es_maximo():
+    """
+    Se buscará el numero mayor de una secuencia de numeros ingresada
+    y retornarlo
+    """
+    secuencia = [20, 32, -1, 1]
+    prueba = es_maximo(secuencia)
+    interno = max(secuencia)
+    assert prueba == interno, "Resultado incorrecto"
+
+
+def test_es_minimo():
+    """
+    Se buscará el numero menor de una secuencia de numeros ingresada
+    y retornarlo
+    """
+    secuencia = [33,91,22,22]
+    prueba = es_minimo(secuencia)
+    interno = min(secuencia)
     assert prueba == interno, "Resultado incorrecto"
